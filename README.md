fb4e1264d2fd0e7bc59cb93451a034ddb1ef1f7d
Gaëtan <gaetan.serre@u-psud.fr>
2020-05-17 17:11:41 +0200

    First algorithm

--- README.md
+++ README.md
@@ -6,7 +6,25 @@ Algorithms for grammar analysis in Python
 Run `pip3 install -r requirements.txt`
 
 # Usage
-Run `python3 grammar_algos.py` and follow instructions on the terminal
+Run `python3 grammar_algos.py -f [path of the file containing the grammar]` and follow instructions on the terminal
 
 # Algorithms implemented
 -[CYK algorithm](https://en.wikipedia.org/wiki/CYK_algorithm)
+-[First algorithm (for an LL parser)](https://en.wikipedia.org/wiki/LL_parser)
+
+# Template of grammar
+E -> T EP
+EP -> '+' T EP | 'eps'
+T -> F TP
+TP -> '*' F TP | 'eps'
+F -> '(' E ')' | 'id'
+
+
+S -> NP VP
+PP -> P NP
+NP -> Det N | NP PP
+VP -> V NP | VP PP
+Det -> 'the'
+N -> 'kids' | 'box' | 'floor'
+V -> 'opened'
+P -> 'on'
--- src/grammar_algos.py
+++ src/grammar_algos.py
@@ -1,14 +1,20 @@
 import nltk
+import argparse
 
-def trace(cyk):
-    for i in range(len(cyk)):
-        print(cyk[i])
+eps = 'ε'
 
 def load_grammar(filename):
     f = open(filename, 'r')
     return nltk.CFG.fromstring(f.read())
 
+##Implementation of CYK algorithm
 def CYK(grammar, word, solve=True):
+
+    def trace_cyk(cyk):
+        for i in range(len(cyk)):
+            print(cyk[i])
+
+    ##Init
     size_word = len(word)
     cyk = [['ø' for i in range(size_word)] for j in range(size_word)]
 
@@ -16,6 +22,7 @@ def CYK(grammar, word, solve=True):
         prod = grammar.productions(rhs=word[i])
         cyk[i][i] = [i.lhs() for i in prod]
     
+    ##Iterations
     if solve:
         index = {}
         for prod in grammar.productions():
@@ -32,25 +39,121 @@ def CYK(grammar, word, solve=True):
                                     cyk[i][j] = []
                                 if index[(B,C)] not in cyk[i][j]:
                                     cyk[i][j].append(index[(B,C)])
-    trace(cyk)
+    trace_cyk(cyk)
     try:
         return grammar.productions()[0].lhs() in cyk[0][size_word-1]
     except:
         return False
 
+##Implementation of first algorithm for an LL parser
+def first(grammar, solve=True):
+
+    def add_first(x, y):
+        add = False
+        if eps not in index[y]:
+            if index[x.lhs()] != {''}:
+                t = index[x.lhs()]
+                index[x.lhs()] = index[x.lhs()] | index[y]
+                if index[x.lhs()] != t:
+                    add = True
+            else:
+                index[x.lhs()] = index[y]
+                add = True
+        return add
+
+    def trace_first(index):
+        for key in index.keys():
+            if not isinstance(key, str):
+                print("First(" + str(key) + ") =",index[key])
+
+
+    ##Init
+    index = {}
+    for prod in grammar.productions():
+        if eps in list(prod.rhs()):
+            index[prod.lhs()] = set([eps])
+            
+        c = prod.lhs()
+        if c not in index:
+            index[c] = set([''])
+
+        for c in list(prod.rhs()):
+            if isinstance(c, str):
+                index[c] = set([c])
+
+    ##Iteration
+    if solve:
+        add = True
+        while(add):
+            add = False
+            for S in grammar.productions():
+                r = list(S.rhs())
+
+                ##First rule
+                add = add_first(S, r[0]) or add ##If add is True, we need to keep it True
+
+                ##Second rule
+                for j in range(1, len(r)):
+                    add_before = True
+                    for i in range(j):
+                        if eps not in index[r[i]]:
+                            add_before = False
+                            break
+                    if add_before:
+                        add = add_first(S, r[j]) or add ##If add is True, we need to keep it True
+
+                ##Third rule
+                add_eps = True
+                for i in range(len(r)):
+                    if eps not in index[r[i]]:
+                        add_eps = False
+                        break
+
+                if add_eps:
+                    if index[S.lhs()] != {''}:
+                        t = index[S.lhs()]
+                        index[S.lhs()] = index[S.lhs()] | {eps}
+                        if index[S.lhs()] != t:
+                            add = True
+                    else:
+                        index[S.lhs()] = {eps}
+                        add = True
+    trace_first(index)
+    return index
+    
 
 
 
 if __name__ == "__main__":
-    grammar = load_grammar('grammar.txt')
-    algo = input("Which algorithm do you want to use?\n").upper()
 
-    if algo == "CYK":
-        word = input("Type the word to analyze (or type eg to use the example)\n")
-        if word == "eg":
-            word = "the kids opened the box on the floor"
-        print('CYK algorithm for',word,': \n')
-        if CYK(grammar, word.split(), solve=True):
-            print(word,"is in the grammar")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", type=str, help="the path of the text file containing the grammar")
+    args = parser.parse_args()
+
+    grammar = load_grammar(args.file)
+
+    it = True
+    while(it):
+        it = False
+        algo = input("Which algorithm do you want to use?\n").upper()
+
+        if algo == "CYK":
+            word = input("Type the word to analyze (or type eg to use the example)\n")
+            if word == "eg":
+                word = "the kids opened the box on the floor"
+            print('\nCYK algorithm for',word,': \n')
+            if CYK(grammar, word.split(), solve=True):
+                print(word,"is in the grammar")
+            else:
+                print(word,"is not in the grammar")
+        
+        elif algo == "FIRST":
+            print("\nThe first table for this grammar is: ")
+            first(grammar,solve=True)
+        
         else:
-            print(word,"is not in the grammar")
+            print("Unknown algorithm. The algorithms currently implemented are: ")
+            print("-CYK")
+            print("-First")
+            print('\n')
+            it = True
